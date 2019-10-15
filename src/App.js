14f2cf4f895fc5f9af1b730fce721e4648a2bc78
mjun413 <48771142+mjun413@users.noreply.github.com>
2019-10-15 10:34:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,45 +2,14 @@ import React, { Component } from 'react';
 import slugify from 'slugify';
 import './App.css';
 import PageTitle from './PageTitle/PageTitle';
-import CustomizerBody from './CustomizerBody/CustomizerBody';
+import PageBody from './PageBody/PageBody';
 
 class App extends Component {
-  state = {
-    selected: {
-      Processor: {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
-        cost: 700
-      },
-      'Operating System': {
-        name: 'Ubuntu Linux 16.04',
-        cost: 200
-      },
-      'Video Card': {
-        name: 'Toyota Corolla 1.5v',
-        cost: 1150.98
-      },
-      Display: {
-        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-        cost: 1500
-      }
-    }
-  };
-
-  updateFeature = (feature, newValue) => {
-    const selected = Object.assign({}, this.state.selected);
-    selected[feature] = newValue;
-    this.setState({
-      selected
-    });
-  };
-
   render() {
     return (
       <div className='App'>
         <PageTitle />
-        <CustomizerBody 
-        features={this.props.features}
-        handleFeatureUpdate={(feature, newValue)=> this.updateFeature(feature, newValue)} />
+        <PageBody features={this.props.features} />
       </div>
     );
   }
